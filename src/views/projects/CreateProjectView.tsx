4a7dd7fd1b50fe15093413bb4dc4b32ffb573246
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-10 19:27:26 -0300

    chore: Update npm dependencies to include react-hook-form

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,22 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const CreateProjectView = () => {
+  const initialValues = {
+    projectName: "",
+    clientName: "",
+    description: "",
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+  const handleForm = (data) => {
+    console.log(data);
+  };
   return (
     <>
-      <h1 className="text-5xl font-black">Crear Tarea</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">
-        Completa el siguiente formulario para crear una nueva tarea
-      </p>
-      <nav className="my-5">
-        <Link
-          to="/"
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-5xl font-black">Crear Tarea</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Completa el siguiente formulario para crear una nueva tarea
+        </p>
+        <nav className="my-5">
+          <Link
+            to="/"
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          >
+            Volver a Tareas
+          </Link>
+        </nav>
+        <form
+          onSubmit={handleSubmit(handleForm)}
+          noValidate
+          className="mt-10 bg-white shadow-lg p-10 rounded-lg"
         >
-          Volver a Tareas
-        </Link>
-      </nav>
+          <input
+            type="submit"
+            value={"Crear Tarea"}
+            className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors rounded-lg"
+          />
+        </form>
+      </div>
     </>
   );
-}
+};
 
-export default CreateProjectView
+export default CreateProjectView;
