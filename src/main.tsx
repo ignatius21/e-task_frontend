2e0b4fd2a2bfda60545c8b90b3141ad0647931c8
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-12 21:50:13 -0300

    chore: Update npm dependencies to include @tanstack/react-query library

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient,QueryClientProvider} from '@tanstack/react-query'
 import './index.css'
 import Router from './route'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router/>
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
