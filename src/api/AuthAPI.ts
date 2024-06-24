67233948ea417a31777abbd2cb828335d8b0270e
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-24 20:04:36 -0300

    feat: Agregar función de inicio de sesión en AuthAPI y vista de inicio de sesión en LoginView

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from '@/lib/axios';
 import { isAxiosError } from 'axios';
-import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from '../types';
+import { ConfirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from '../types';
 
 export async function createAccount(formData: UserRegistrationForm) {
   try {
@@ -33,6 +33,18 @@ export async function requestConfirmationCode(formData: RequestConfirmationCodeF
   } catch (error) {
     if (isAxiosError(error)) {
       throw error.response?.data;
+    }
+  }
+}
+
+export async function login(formData: UserLoginForm) {
+  try {
+    const url = '/auth/login';
+    const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
     }
   }
 }
--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -2,6 +2,9 @@ import { useForm } from "react-hook-form";
 import { UserLoginForm } from "@/types/index";
 import ErrorMessage from "@/components/ErrorMessage";
 import { Link } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { login } from "@/api/AuthAPI";
+import { toast } from "react-toastify";
 
 export default function LoginView() {
   const initialValues: UserLoginForm = {
@@ -14,7 +17,19 @@ export default function LoginView() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const handleLogin = (formData: UserLoginForm) => {};
+  const { mutate } = useMutation({
+    mutationFn: login,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data?.message);
+    },
+  })
+
+  const handleLogin = (formData: UserLoginForm) => {
+    mutate(formData);
+  };
 
   return (
     <>
