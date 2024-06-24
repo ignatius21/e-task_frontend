4d388c458a6bbe9508c2e3114661ed3e963a25de
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-24 06:24:56 -0300

    feat: Actualizar API de autenticación para confirmar cuenta de usuario

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,16 +1,27 @@
 import api from '@/lib/axios';
 import { isAxiosError } from 'axios';
-import { UserRegistrationForm } from '../types';
+import { ConfirmToken, UserRegistrationForm } from '../types';
 
 export async function createAccount(formData: UserRegistrationForm) {
   try {
     const url = '/auth/create-account';
-    const {data} = await api.post(url, formData);
+    const {data} = await api.post<string>(url, formData);
     return data;
   } catch (error) {
     if (isAxiosError(error)) {
       throw error.response?.data;
     }
-    throw error;
+  }
+}
+
+export async function confirmAccount(formData: ConfirmToken) {
+  try {
+    const url = '/auth/confirm-account';
+    const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw error.response?.data;
+    }
   }
 }
--- src/views/auth/ConfirmAccountView.tsx
+++ src/views/auth/ConfirmAccountView.tsx
@@ -2,15 +2,35 @@ import { Link } from "react-router-dom";
 import {PinInput,PinInputField} from '@chakra-ui/pin-input';
 import { useState } from "react";
 import { ConfirmToken } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+import { confirmAccount } from "@/api/AuthAPI";
+import { toast } from "react-toastify";
 
 
 export default function ConfirmAccountView() {
+
     const [token, setToken] = useState<ConfirmToken['token']>('')
-    const handleChange = (token: ConfirmToken['token']) => setToken(token)
+
+    const {mutate} = useMutation({
+        mutationFn: confirmAccount,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+        }
+    })
+
+
+    const handleChange = (token: ConfirmToken['token']) => {
+        setToken(token)
+    }
+
+
 
     const handleComplete = (token: ConfirmToken['token']) => {
-        console.log(token)
-    }
+        mutate({token})
+    } 
     
     
   return (
