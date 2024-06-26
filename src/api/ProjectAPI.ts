e875bd0c01b1042ab4e147882c49a040460e3a45
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-26 20:01:01 -0300

    Refactorizar API de proyectos para eliminar el uso innecesario de token de autenticación

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -19,14 +19,8 @@ export async function createProject(formData: ProjectFormData) {
 // Obtener un listado de proyectos
 
 export async function getProjects() {
-    const token = localStorage.getItem('AUTH_TOKEN');
     try {
-        const { data } = await api('/projects',{
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        
-        });
+        const { data } = await api('/projects');
         const response = Dashboard.safeParse(data)
         if(response.success) {
             return response.data;
