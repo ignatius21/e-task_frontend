2079926f4a7777021c263556b566cffe0a891b6a
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-01 05:51:07 -0300

    feat: Agregar tipo de formulario UserProfileForm

--- src/types/index.ts
+++ src/types/index.ts
@@ -30,6 +30,8 @@ export const userSchema = authSchema.pick({
 
 export type User = z.infer<typeof userSchema>;
 
+export type UserProfileForm = Pick<User, 'name' | 'email'>;
+
 
 // Task schema
 export const taskStatusSchema = z.enum(['pending', 'onHold', 'inProgress', 'underReview', 'completed']);
--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -1,6 +1,9 @@
+import ProfileForm from "@/components/profile/ProfileForm";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function ProfileView() {
-  return (
-    <div>ProfileView</div>
-  )
+    const {data, isLoading} = useAuth();
+
+    if(isLoading) return <div>Loading...</div>
+    if(data) return <ProfileForm data={data}/>
 }
