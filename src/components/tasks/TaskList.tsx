25d2152d18acfc12302232604b2e7ce140443b61
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-01 20:25:07 -0300

    feat: Agregar soporte para arrastrar y soltar tareas en TaskList

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,8 +1,12 @@
-import { Task } from "@/types/index";
+import { Task, TaskStatus } from "@/types/index";
 import TaskCard from "./TaskCard";
 import { statusTranslations } from "@/locales/es";
 import DropTask from "./DropTask";
 import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateTaskStatus } from "@/api/TaskAPI";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
 
 type TaskListProps = {
   tasks: Task[];
@@ -30,6 +34,26 @@ const statusColors: { [key: string]: string } = {
 };
 
 export default function TaskList({ tasks, canEdit }: TaskListProps) {
+
+
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const queryClient = useQueryClient();
+  const {mutate} = useMutation({
+    mutationFn: updateTaskStatus,
+    onError: (error: Error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({queryKey: ['editProject', projectId]});
+      toast.success(data);
+    }
+  });
+
+
+
+
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
     currentGroup = [...currentGroup, task];
@@ -39,7 +63,9 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
   const handleDragEnd = (e:DragEndEvent) => {
     const {active, over} = e;
     if (over && over.id) {
-      console.log('valido')
+      const taskId = active.id.toString();
+      const status = over.id as TaskStatus;
+      mutate({projectId,taskId,status})
     } 
   }
 
