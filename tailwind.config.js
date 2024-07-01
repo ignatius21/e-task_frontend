f7ff5590e4802d61546ec6869ae6ac40372b1f7e
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-01 05:41:19 -0300

    feat: Agregar soporte para Tailwind CSS Forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
 export default {
   content: [
     './index.html',
@@ -7,6 +8,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  
+  plugins: [
+    forms,
+  ],
 }
 
