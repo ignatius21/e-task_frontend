b2138057087daf0f34c44a84d98ce47140693572
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-03 20:36:58 -0300

    feat: Improve error message component with auto-hide functionality

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,6 +1,24 @@
+import React, { useState, useEffect } from "react";
+
 const ErrorMessage = ({ children }: { children: React.ReactNode }) => {
+  const [showError, setShowError] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowError(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <div className="text-center my-4 bg-red-100 text-red-600 font-bold p-3 uppercase text-sm">
+    <div
+      className={`rounded-2xl text-center my-2 bg-red-100 text-red-600 font-bold p-3 uppercase text-sm ${
+        showError ? "" : "hidden"
+      }`}
+    >
       {children}
     </div>
   );
