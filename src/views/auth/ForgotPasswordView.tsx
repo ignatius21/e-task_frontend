c863f78a8ff2986367e0786b25c420bc0abe2b23
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-04 06:03:44 -0300

    feat: Agregar validación de email y estilos al formulario de recuperación de contraseña

--- src/views/auth/ForgotPasswordView.tsx
+++ src/views/auth/ForgotPasswordView.tsx
@@ -5,6 +5,7 @@ import ErrorMessage from "@/components/ErrorMessage";
 import { useMutation } from "@tanstack/react-query";
 import { forgotPassword } from "@/api/AuthAPI";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 export default function ForgotPasswordView() {
   const initialValues: ForgotPasswordForm = {
@@ -32,60 +33,73 @@ export default function ForgotPasswordView() {
     mutate(formData);
   };
 
+  const [isEmailFocused, setIsEmailFocused] = useState(false);
+
   return (
     <>
       <h1 className="text-5xl font-black text-white">Reestablecer Password</h1>
       <p className="text-2xl font-light text-white mt-5">
         Completa el email para {""}
-        <span className=" text-fuchsia-500 font-bold"> solicitar un nuevo password</span>
+        <span className=" text-blue-500 font-bold"> solicitar un nuevo password</span>
       </p>
       <form
         onSubmit={handleSubmit(handleForgotPassword)}
-        className="space-y-8 p-10 mt-10 bg-white"
+        className="space-y-8 p-10 mt-10 bg-white rounded-2xl"
         noValidate
       >
         <div className="flex flex-col gap-5">
-          <label className="font-normal text-2xl" htmlFor="email">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Email de Registro"
-            className="w-full p-3  border-gray-300 border"
-            {...register("email", {
-              required: "El Email de registro es obligatorio",
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "E-mail no válido",
-              },
-            })}
-          />
+          <label className="font-normal text-2xl">Email</label>
+
+          <div className="relative flex items-center">
+            <input
+              id="email"
+              type="email"
+              placeholder="Email de Registro"
+              className="w-full p-3 border-slate-200 border rounded-2xl pl-10"
+              {...register("email", {
+                required: "El Email es obligatorio",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "E-mail no válido",
+                },
+              })}
+              onFocus={() => setIsEmailFocused(true)}
+              onBlur={() => setIsEmailFocused(false)}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke={isEmailFocused ? "blue" : "currentColor"}
+              className="absolute left-2 h-6 w-6 text-gray-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+              />
+            </svg>
+          </div>
           {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </div>
 
         <input
           type="submit"
           value="Enviar Instrucciones"
-          className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3  text-white font-black  text-xl cursor-pointer"
+          className="bg-blue-700 hover:bg-blue-500 w-full p-3  text-white font-black  text-xl cursor-pointer rounded-2xl"
         />
+          <nav className="mt-10 flex flex-col space-y-4">
+          <Link
+            to={"/auth/login"}
+            className="text-center text-gray-400 font-normal"
+          >
+            Ya tienes una cuenta?{" "}
+            <span className="text-blue-500 font-bold">Inicia sesion</span>
+          </Link>
+        </nav>
       </form>
 
-      <nav className="mt-10 flex flex-col space-y-4">
-        <Link
-          to="/auth/login"
-          className="text-center text-gray-300 font-normal"
-        >
-          ¿Ya tienes cuenta? Iniciar Sesión
-        </Link>
-
-        <Link
-          to="/auth/register"
-          className="text-center text-gray-300 font-normal"
-        >
-          ¿No tienes cuenta? Crea una
-        </Link>
-      </nav>
     </>
   );
 }
