4124a837a0baecc3f799cebc4c88bfe8900b5ae8
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-05 06:52:21 -0300

    Actualizacion del header

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,4 @@
 import { Outlet,Navigate } from "react-router-dom";
-import Logo from "@/components/Logo";
 import NavMenu from "@/components/NavMenu";
 import {Slide, ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,8 +15,8 @@ const AppLayout = () => {
     <>
       <header className="bg-gray-800 py-5">
         <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
-          <div className="w-64">
-            <Logo />
+          <div className="flex">
+          <p className="font-bold text-5xl mt-3 text-gray-400">E -<span className="text-sky-700">Task</span></p>
           </div>
           <NavMenu 
             name={data.name}
