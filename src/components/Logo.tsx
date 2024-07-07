e80f038e9c30286f746b588f281f6ab3bbe4a131
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-07 12:19:43 -0300

    chore: Actualizar estilos del campo de email en Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <Link to={'/'} className=" flex justify-center flex-col items-center">
       <img src="/images/dashboardLogo.png" alt="logotipo e-task" className=" w-[180px]"/>
-      <p className="font-bold text-5xl mt-3 text-gray-400">E -<span className="text-sky-700">Task</span></p>
+      <p className="font-bold text-5xl mt-3 text-gray-400">E -<span className="text-blue-300">Task</span></p>
     </Link>
   )
 }
