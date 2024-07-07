6478153b74bc72a49b76a3dd53d1ac7bcd73f710
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-07 13:56:17 -0300

    chore: Actualizar estilos del campo de email en NavbarMenu.tsx

--- src/components/NavbarMenu.tsx
+++ src/components/NavbarMenu.tsx
@@ -33,7 +33,7 @@ export default function NavbarMenu() {
   };
 
   return (
-    <header className="bg-white">
+    <header className="bg-gray-800">
       <nav
         aria-label="Global"
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
@@ -49,7 +49,7 @@ export default function NavbarMenu() {
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="h-6 w-6" />
@@ -59,13 +59,28 @@ export default function NavbarMenu() {
           <Popover className="relative">
             <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
               <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                <p className="text-sm font-semibold leading-6 text-gray-900">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-6 mr-3 text-slate-400"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                  />
+                </svg>
+
+                <p className="text-sm font-semibold leading-6 text-slate-300">
                   {data?.name}
                 </p>
               </div>
               <ChevronDownIcon
                 aria-hidden="true"
-                className="h-5 w-5 flex-none text-gray-400"
+                className="h-5 w-5 flex-none text-sky-300"
               />
             </PopoverButton>
             <PopoverPanel
@@ -90,7 +105,7 @@ export default function NavbarMenu() {
                           d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                         />
                       </svg>
-                      <p className="block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      <p className="block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-500">
                         Perfil
                       </p>
                     </div>
@@ -114,7 +129,7 @@ export default function NavbarMenu() {
                         />
                       </svg>
 
-                      <p className="block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      <p className="block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-500">
                         Cerrar sesion
                       </p>
                     </div>
@@ -135,7 +150,7 @@ export default function NavbarMenu() {
           <div className="flex items-center justify-between">
             <div className="-m-1.5 p-1.5">
               <p className="font-bold text-2xl">
-                E-<span className="text-blue-800">Task</span>
+                E-<span className="text-blue-300">Task</span>
               </p>
             </div>
             <button
