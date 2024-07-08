7137b488c71d0536c5d802f1dae71d3ca68148a3
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-08 06:31:45 -0300

    chore: Actualizar estilos del campo de email en varios archivos

--- src/components/projects/DeleteProjectModal.tsx
+++ src/components/projects/DeleteProjectModal.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from "@headlessui/react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -50,6 +50,8 @@ export default function DeleteProjectModal() {
     await deleteProjectMutation.mutateAsync(deleteProjectId);
 };
 
+const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+
   return (
     <Transition appear show={show} as={Fragment}>
       <Dialog
@@ -81,19 +83,19 @@ export default function DeleteProjectModal() {
               leaveTo="opacity-0 scale-95"
             >
               <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                <DialogTitle as="h3" className="font-black text-4xl  my-5">
+                <DialogTitle as="h3" className="text-gray-800 font-bold text-4xl  my-5">
                   Eliminar Proyecto{" "}
                 </DialogTitle>
 
-                <p className="text-xl font-bold">
+                <p className="text-xl text-red-500 font-bold">
                   Confirma la eliminación del proyecto {""}
-                  <span className="text-fuchsia-600">
+                  <span className="text-gray-700">
                     colocando tu password
                   </span>
                 </p>
 
                 <form
-                  className="mt-10 space-y-5"
+                  className="mt-10 space-y-5 rounded-3xl"
                   onSubmit={handleSubmit(handleForm)}
                   noValidate
                 >
@@ -101,15 +103,33 @@ export default function DeleteProjectModal() {
                     <label className="font-normal text-2xl" htmlFor="password">
                       Password
                     </label>
-                    <input
-                      id="password"
-                      type="password"
-                      placeholder="Password Inicio de Sesión"
-                      className="w-full p-3  border-gray-300 border"
-                      {...register("password", {
-                        required: "El password es obligatorio",
-                      })}
-                    />
+                    <div className="relative flex items-center">
+                      <input
+                        id="password"
+                        type="password"
+                        placeholder="Password Inicio de Sesión"
+                        className="input w-full p-3 border-slate-200 border rounded-2xl pl-10 focus:ring-0"
+                        {...register("password", {
+                          required: "El password es obligatorio",
+                        })}
+                        onFocus={() => setIsPasswordFocused(true)}
+                        onBlur={() => setIsPasswordFocused(false)}
+                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke={isPasswordFocused ? "#BDE9D4" : "currentColor"}
+                        className="absolute left-3 top-3 h-6 w-6 text-slate-400"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33"
+                        />
+                      </svg>
+                    </div>
                     {errors.password && (
                       <ErrorMessage>{errors.password.message}</ErrorMessage>
                     )}
@@ -117,7 +137,7 @@ export default function DeleteProjectModal() {
 
                   <input
                     type="submit"
-                    className=" bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3  text-white font-black  text-xl cursor-pointer"
+                    className="beautiful-button3 w-full"
                     value="Eliminar Proyecto"
                   />
                 </form>
