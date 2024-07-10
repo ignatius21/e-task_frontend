d7a64fa6856b6a1458968ef17510e28466f9f46b
ignatius21 <ignaciojosealberto@hotmail.com>
2024-07-10 06:01:34 -0300

    chore: Actualizar estilos del campo de email en DashboardView para mejorar la apariencia del formulario de vista del panel de control

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -32,7 +32,7 @@ const DashboardView = () => {
   if (data)
     return (
       <>
-        <h1 className="text-5xl font-black">Mis Tareas</h1>
+        <h1 className="text-5xl text-slate-600 font-bold">Mis Tareas</h1>
         <p className="text-2xl font-light text-gray-500 mt-5">
           Maneje y administre sus trabajos
         </p>
@@ -56,17 +56,51 @@ const DashboardView = () => {
                     <p className="text-gray-500">{project.clientName}</p>
                     <p className="text-gray-500">{project.description}</p>
                     <div className="flex min-w-0">
-                      <p
+                      <div
                         className={`font-bold ${
                           project.manager === user?._id
                             ? "bg-red rounded-3xl px-3 text-white py-2"
-                            : "bg-green-400 rounded-lg px-2 text-gray-700 py-1"
+                            : "bg-sky-500 rounded-3xl px-3 text-white py-2"
                         }`}
                       >
-                        {project.manager === user?._id
-                          ? "Manager"
-                          : "Colaborador"}
-                      </p>
+                        {project.manager === user?._id ? (
+                          <div className="relative flex items-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                              />
+                            </svg>
+                            <p className="ml-1">Manager</p>
+                          </div>
+                        ) : (
+                          <div className="relative flex items-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+                              />
+                            </svg>
+                            <p className="ml-2">Colaborador</p>
+                          </div>
+                        )}
+                      </div>
                     </div>
                   </div>
                   {/* Columna de opciones del proyecto */}
@@ -93,7 +127,7 @@ const DashboardView = () => {
                             <MenuItem>
                               <Link
                                 to={`/project/${project._id}/edit`}
-                                className="block px-3 py-1 text-sm leading-6 text-gray-400 font-bold hover:text-green"
+                                className="block px-3 py-1 text-sm leading-6 text-gray-400 font-bold hover:text-sky-500"
                               >
                                 Editar Proyecto
                               </Link>
